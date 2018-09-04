e1a090e536d18fb615374bc14ec4de5518f036ed
Huey <leejinhuey@gmail.com>
2018-09-04 16:23:12 +0800

    added generatePermutations

--- web/tt2.js
+++ web/tt2.js
@@ -25,6 +25,32 @@ const parseInput = ({ input }) => {
   return output
 }
 
-const input = '(a and b) and (c and (d or e))'
+const generatePermutations = (variables = ['a', 'b', 'c']) => {
+  let output = []
+  for(let rows = 0; rows < Math.pow(2, variables.length); rows++){
+    const combination = {}
+    for(let i = 0; i < variables.length; i++){
+      combination[variables[i]] = (rows * i) % 2 === 0
+    }
+    output.push(combination)
+  }
+  return output
+}
+
+const input = '(a and b) and not (c and (d or e))'
 
 console.log(JSON.stringify(parseInput({ input }), null, 2))
+console.log(generatePermutations())
+
+/*
+
+  [ { a: false, b: false, c: false },
+  { a: true, b: false, c: false },
+  { a: false, b: true, c: false },
+  { a: true, b: true, c: false },
+  { a: false, b: false, c: true },
+  { a: true, b: false, c: true },
+  { a: false, b: true, c: true },
+  { a: true, b: true, c: true } ]
+
+*/
